d4ba1dd899406456c4732c8762a3f82af367c04f
Kethillyn <naykethillyn@gmail.com>
2024-08-18 22:39:15 -0300

    API com Django REST

---
+++ escola/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from escola.models import Estudante, Curso, Matriculas
+
+class EstudanteSerializer(serializers.ModelSerializer):
+          class Meta:
+                    model = Estudante
+                    fields = '__all__'
+
+class CursoSerializer(serializers.ModelSerializer):
+          class Meta:
+                    model = Curso
+                    fields = '__all__'
+
+class MatriculaSerializer(serializers.ModelSerializer):
+          class Meta:
+                    model = Matriculas
+                    exclude = []
+
+class ListaMatriculasEstudanteSerializer( serializers.ModelSerializer):
+          curso = serializers.ReadOnlyField(source = 'curso.descricao')
+          periodo = serializers.SerializerMethodField()
+          class Meta:
+                    model = Matriculas
+                    fields = ['curso','periodo']
+          def get_periodo(self,obj):
+                    return obj.get_periodo_display()
+
+class ListaMatriculasCursoSerializer(serializers.ModelSerializer):
+    estudante_nome = serializers.ReadOnlyField(source = 'estudante.nome')
+    class Meta:
+        model = Matriculas
+        fields = ['estudante_nome']
